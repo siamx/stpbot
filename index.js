0d3faddf3ef7c1b709844d13e140285bcd54e2ec
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-05-17 14:23:48 +0200

    reformatted

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
-})
+});
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
@@ -22,46 +22,43 @@ app.get('/webhook/', function (req, res) {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
-})
+});
 
 // Spin up the server
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('running on port', app.get('port'))
-})
-
-
+});
 
 
 app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
+    messaging_events = req.body.entry[0].messaging;
     for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
         if (event.message && event.message.text) {
-            text = event.message.text
+            text = event.message.text;
             sendTextMessage(sender, "معلش :)")//+text.substring(0, 200))
         }
     }
     res.sendStatus(200)
-})
+});
 
 var token = "EAAGF4NJNA9EBAAgP9J9LVqndu52KzRpYrqx3PplvxS20KgXKe2sq39EyaHfLGp5mhdSIRvzuPiGOPT4M7xQZAGdvLuUZBlHs8ytUy0uGgChguiZBPfaYQzqZAIjbjfR2ZBsaeth7MYySGAcBZBMsEgozLcDAT8Q3iBORR4Yck3RQZDZD"
 
 
-
 function sendTextMessage(sender, text) {
     messageData = {
-        text:text
-    }
+        text: text
+    };
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token: token},
         method: 'POST',
         json: {
-            recipient: {id:sender},
-            message: messageData,
+            recipient: {id: sender},
+            message: messageData
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
